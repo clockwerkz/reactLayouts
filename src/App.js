7e653bc870c35f4c67d353f4504232749aac1ddf
clockwerkz <clockwerkz@gmail.com>
2019-07-17 21:31:55 -0700

    adds window resizing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState  } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [ width, setWidth ] = useState(window.innerWidth);
+  const [ panel, setPanel ] = useState('panel2');
+  const handleWindowChange = ()=> setWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowChange);
+    return function cleanup() {
+      window.removeEventListener('resize', handleWindowChange);
+    }
+  }, []);
+  
+  
+  const renderSelectedPanel = () => {
+    switch(panel) {
+      case 'panel3':
+          return (
+            <div className="panel">
+            <h3>Panel #3</h3>
+          </div>
+          )
+      case 'panel2':
+          return (
+            <div className="panel">
+            <h3>Panel #2</h3>
+          </div>
+          )
+      case 'panel1':
+          return (
+            <div className="panel">
+            <h3>Panel #1</h3>
+          </div>
+          )
+      default: 
+          return (
+            <div>
+              <p>No Panel Selected</p>
+            </div>
+          )
+    }
+  }
+  
+  const isMobile = width < 501;
+  const currentPanel = renderSelectedPanel();
+  if (isMobile) {
+    return (
+      <div className="App">
+        <ul className="button__wrapper">
+          <li className="button button--blue" onClick={()=>setPanel('panel1')}>Panel #1</li>
+          <li className="button button--green" onClick={()=>setPanel('panel2')}>Panel #2</li>
+          <li className="button button--yellow" onClick={()=>setPanel('panel3')}>Panel #3</li>  
+        </ul>
+        <div className="panel__wrapper">
+          {currentPanel}
+        </div>
+      </div>)
+    } else {
+    return (
+    <div className="panel__wrapper">
+      <div className="panel">
+          <h3>Panel #1</h3>
+        </div>
+        <div className="panel">
+          <h3>Panel #2</h3>
+        </div>
+        <div className="panel">
+          <h3>Panel #3</h3>
+        </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
